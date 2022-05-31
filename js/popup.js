868ee70cea3827338d96a87d8a27089fcf3ac218
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-05-31 17:25:09 +0200

    contacto creado

--- js/popup.js
+++ js/popup.js
@@ -1,16 +1,13 @@
-const popup = document.querySelector('.popup-wrapper');
+const popup = document.querySelector('.popup_box');
 const close = document.querySelector('.popup-close');
 
+
 function pop_up() {
-    popup.style.display = 'block';
 
-    close.addEventListener('click', () => {
-        popup.style.display = 'none';
+    $(document).ready(function () {
+        $('.popup_box').css("display", "block");
     });
-
-    popup.addEventListener('click', e => {
-        if (e.target.className == 'popup-wrapper' || 'popup-close') {
-            popup.style.display = 'none';
-        }
+    $('.btn1').click(function () {
+        $('.popup_box').css("display", "none");
     });
 }
