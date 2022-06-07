803c0e3a7e7ac1396a4fb67c81b07e5ed7cd0b85
pablodt <pablo.duran.t@uma.es>
2022-06-07 17:51:24 +0200

    Intento de pistas random

--- js/pistas.js
+++ js/pistas.js
@@ -7,62 +7,102 @@ var random1 = getRandomArbitrary(1, 3);
 var random2 = getRandomArbitrary(1, 3);
 var random3 = getRandomArbitrary(1, 3);
 
+let selected_pista;
+
+let disponibleP1 = true;
+let disponibleP2 = true;
+let disponibleP3 = true;
+
 function getRandomArbitrary(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function listarPistas(indice) {
+function listarPistas() {
+    pista1.innerHTML = '';
+    pista2.innerHTML = '';
+    pista3.innerHTML = '';
+
+
     if (random1.toString() == "1") {
-        
+        disponibleP1 = false;
+    } else if (random1.toString() == "2") {
+        disponibleP2 = false;
+    } else if (random1.toString() == "3") {
+        disponibleP3 = false;
+    }
+
+    if (random2.toString() == "1") {
+        disponibleP1 = false;
     } else if (random2.toString() == "2") {
-        
+        disponibleP2 = false;
+    } else if (random2.toString() == "3") {
+        disponibleP3 = false;
     }
 
-
-    time_element1.innerHTML = '';
-    time_element2.innerHTML = '';
-    time_element3.innerHTML = '';
-    let amount_hour = 12;
-
-    for (let i = 0; i < amount_hour; i++) {
-        const hour_element = document.createElement('buttom');
-        hour_element.classList.add('boton-fecha');
-
-        if ((i == random1 || i == random2 || i == random3)) {
-            hour_element.classList.add('disabled');
-            hour_element.setAttribute('disabled', true);
-        }
-
-        hour_element.textContent = i + 9 + ":00";
-
-
-        if (indice == i) {
-            hour_element.classList.add('selected');
-        }
-
-        hour_element.addEventListener('click', function () {
-            if (hour_element.getAttribute('disabled') != "true") {
-                if (indice == i) {
-                    indice = -1;
-                    seleccionado = false;
-                } else {
-                    seleccionado = true;
-                    indice = i;
-                }
-                cambiarHora(indice);
-                listarBotones(indice);
-            }
-        });
-
-        if (i >= 0 && i <= 3) {
-            time_element1.appendChild(hour_element);
-        } else if (i >= 4 && i <= 7) {
-            time_element2.appendChild(hour_element);
-        } else {
-            time_element3.appendChild(hour_element);
-        }
-
+    if (random3.toString() == "1") {
+        disponibleP1 = false;
+    } else if (random3.toString() == "2") {
+        disponibleP2 = false;
+    } else if (random3.toString() == "3") {
+        disponibleP3 = false;
     }
+
+    const pista1_element = document.createElement('button');
+    pista1_element.classList.add('btn btn-outline-secondary w-25 m-3 col');
+
+    const pista2_element = document.createElement('button');
+    pista2_element.classList.add('btn btn-outline-secondary w-25 m-3 col');
+
+    const pista3_element = document.createElement('button');
+    pista3_element.classList.add('btn btn-outline-secondary w-25 m-3 col');
+    
+    if (!disponibleP1) {
+        pista1_element.classList.add('disabled');
+        pista1_element.setAttribute('disabled', true);
+    }
+
+    pista1_element.textContent("Pista 1");
+
+    pista1_element.addEventListener('click', function () {
+        if (pista1_element.getAttribute('disabled') != "true") {
+            cambiarPista("1");
+            //listarBotones(indice);
+        }
+    });
+
+    pista1.appendChild(pista1_element);
+
+    if (!disponibleP2) {
+        pista2_element.classList.add('disabled');
+        pista2_element.setAttribute('disabled', true);
+    }
+
+    pista2_element.textContent("Pista 2");
+
+    pista2_element.addEventListener('click', function () {
+        if (pista2_element.getAttribute('disabled') != "true") {
+            cambiarPista("2");
+            //listarBotones(indice);
+        }
+    });
+
+    pista2.appendChild(pista2_element);
+
+    if (!disponibleP3) {
+        pista3_element.classList.add('disabled');
+        pista3_element.setAttribute('disabled', true);
+    }
+
+    pista3_element.textContent("Pista 3");
+
+    pista3_element.addEventListener('click', function () {
+        if (pista3_element.getAttribute('disabled') != "true") {
+            cambiarPista("3");
+            //listarBotones(indice);
+        }
+    });
+
+    pista3.appendChild(pista3_element);
 
 }
 
@@ -72,10 +112,7 @@ function calcularRandom() {
     random3 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
 }
 
-function cambiarHora(hora) {
-    if (hora == -1) {
-        selected_hora = undefined;
-    } else {
-        selected_hora = hora + 9 + ":00";
-    }
+function cambiarPista(pista) {
+    selected_pista = pista;
+    console.log(selected_pista);
 }
--- js/popup_conf.js
+++ js/popup_conf.js
@@ -2,13 +2,17 @@ const popupConf = document.querySelector('.popup_conf');
 const closeConf = document.querySelector('.popup-close');
 
 
-function pop_upConf(hora, dia, pista, val) {
+function pop_upConf(hora, dia, pista, val, val2, texto) {
 
     $(document).ready(function () {
-        $('.popup_conf').css("display", "block");
-        $('.fecha_hora_reserva').text(dia + " a las " + hora);
-        if (val == "1") {
-            $('.pista_reserva').text("Pista " + pista);
+        if (val2 == "1") {
+            $('.popup_conf').css("display", "block");
+            $('.fecha_hora_reserva').text(dia + " a las " + hora);
+            if (val == "1") {
+                $('.pista_reserva').text("Pista " + pista);
+            }
+        } else {
+            $('.fecha_hora_reserva').text(texto);
         }
     });
     $('.btn2').click(function () {
