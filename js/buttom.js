08e8336875903ab4d4d09eb8c196a52cc3f83034
Jose-Tapia-Catena <jmtapiacatena@uma.es>
2022-06-07 07:53:28 +0200

    arreglo de botones

--- js/buttom.js
+++ js/buttom.js
@@ -1,9 +1,11 @@
 const buttom_element = document.querySelector('.boton-fecha');
-const time_element1 = document.querySelector('.ejem');
-const time_element2 = document.querySelector('.ejem');
-const time_element3 = document.querySelector('.ejem');
+const time_element1 = document.querySelector('#col1');
+const time_element2 = document.querySelector('#col2');
+const time_element3 = document.querySelector('#col3');
 
 
+let buttom_hour = -1;
+let selected_hora;
 
 /* // EVENT LISTENERS
 buttom_element.addEventListener('click', toggleButtomPicker);
@@ -22,7 +24,9 @@ function toggleButtomPicker(event) {
 listarBotones();
 
 function listarBotones(e) {
-    time_element.innerHTML = '';
+    time_element1.innerHTML = '';
+    time_element2.innerHTML = '';
+    time_element3.innerHTML = '';
     let amount_hour = 12;
 
     for (let i = 0; i < amount_hour; i++) {
@@ -30,17 +34,38 @@ function listarBotones(e) {
         hour_element.classList.add('boton-fecha');
         hour_element.classList.add('margin-buttom');
 
-        hour_element.textContent = i + 1 + ":00";
+        hour_element.textContent = i + 9 + ":00";
 
-        /*         if (selectedDay == (i + 1) && selectedYear == year && selectedMonth == month) {
-                    hour_element.classList.add('selected');
-                } */
+        if (buttom_hour == i) {
+            hour_element.classList.add('selected');
+        }
 
-        buttom_element.addEventListener('click', function () {
-
+        hour_element.addEventListener('click', function () {
+            if (buttom_hour == i) {
+                buttom_hour = -1;
+            } else {
+                buttom_hour = i;
+            }
+            cambiarHora(buttom_hour);
+            listarBotones();
         });
 
-        time_element.appendChild(hour_element);
+        if (i >= 0 && i <= 3) {
+            time_element1.appendChild(hour_element);
+        } else if (i >= 4 && i <= 7) {
+            time_element2.appendChild(hour_element);
+        } else {
+            time_element3.appendChild(hour_element);
+        }
+
     }
 
+}
+
+function cambiarHora(hora) {
+    if (hora == -1) {
+        selected_hora = undefined;
+    } else {
+        selected_hora = hora + 9 + ":00";
+    }
 }
