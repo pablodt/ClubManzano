9041e4d02a5fcbf4b000e0cbf9b49944c66ee2bc
pablodt <pablo.duran.t@uma.es>
2022-06-18 13:24:08 +0200

    Pádel y tenis

---
+++ js/buttom_tenis.js
@@ -0,0 +1,75 @@
+const buttom_element = document.querySelector('.boton-fecha');
+const time_element1 = document.querySelector('#col1');
+const time_element2 = document.querySelector('#col2');
+const time_element3 = document.querySelector('#col3');
+
+//random
+var random1 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+var random2 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+var random3 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+
+function listarBotones(indice) {
+    time_element1.innerHTML = '';
+    time_element2.innerHTML = '';
+    time_element3.innerHTML = '';
+    let amount_hour = 12;
+
+    for (let i = 0; i < amount_hour; i++) {
+        const hour_element = document.createElement('buttom');
+        hour_element.classList.add('boton-fecha');
+
+        if ((i == random1 || i == random2 || i == random3)) {
+            hour_element.classList.add('disabled');
+            hour_element.setAttribute('disabled', true);
+        }
+
+        hour_element.textContent = i + 9 + ":00";
+
+
+        if (indice == i) {
+            hour_element.classList.add('selected');
+        }
+
+        hour_element.addEventListener('click', function () {
+            if (hour_element.getAttribute('disabled') != "true") {
+                if (indice == i) {
+                    indice = -1;
+                    seleccionado = false;
+                } else {
+                    seleccionado = true;
+                    indice = i;
+                }
+                cambiarHora(indice);
+                listarBotones(indice);
+            }
+        });
+
+        if (i >= 0 && i <= 3) {
+            time_element1.appendChild(hour_element);
+        } else if (i >= 4 && i <= 7) {
+            time_element2.appendChild(hour_element);
+        } else {
+            time_element3.appendChild(hour_element);
+        }
+
+    }
+
+}
+
+function calcularRandom() {
+    random1 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+    random2 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+    random3 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+}
+
+function cambiarHora(hora) {
+    if (hora == -1) {
+        selected_hora = undefined;
+    } else {
+        selected_hora = hora + 9 + ":00";
+        console.log(selected_hora);
+        $.getScript("js/pistas.js", function () {
+            listarPistas();
+        });
+    }
+}
---
+++ js/pistas_tenis.js
@@ -0,0 +1,77 @@
+const pistas = document.getElementById('pistas');
+
+let selected_pista;
+
+pistas.style.display = "none";
+
+// FUNCTIONS
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function listarPistas() {
+    let pista1 = document.querySelector('#pista1');
+    let pista2 = document.querySelector('#pista2');
+
+    pistas.style.display = "inline-block";
+
+    //random
+    var random1 = getRandomArbitrary(1, 4);
+    var random2 = getRandomArbitrary(1, 4);
+    var randomExtra = getRandomArbitrary(1, 3);
+
+    console.log(random1);
+    console.log(random2);
+
+    let disponibleP1 = true;
+    let disponibleP2 = true;
+
+    pista1.innerHTML = '';
+    pista2.innerHTML = '';
+
+    if (random1.toString() == "1") {
+        disponibleP1 = false;
+    } else if (random1.toString() == "2") {
+        disponibleP2 = false;
+    }
+
+    if (random2.toString() == "1") {
+        disponibleP1 = false;
+    } else if (random2.toString() == "2") {
+        disponibleP2 = false;
+    }
+
+    if (!disponibleP1 && !disponibleP2) {
+        if (randomExtra.toString() == "1") {
+            disponibleP1 = true;
+        } else if (randomExtra.toString() == "2") {
+            disponibleP2 = true;
+        }
+    }
+
+    if (!disponibleP1) {
+        pista1.innerHTML = '<button class="btn btn-outline-secondary w-25 m-3 col" disabled>Pista 1</button>';
+    } else {
+        pista1.innerHTML = '<button class="btn btn-outline-secondary w-25 m-3 col">Pista 1</button>';
+        pista1.addEventListener('click', function () {
+            selected_pista = "1";
+            console.log(selected_pista);
+        });
+    }
+
+    if (!disponibleP2) {
+        pista2.innerHTML = '<button class="btn btn-outline-secondary w-25 m-3 col" disabled>Pista 2</button>';
+    } else {
+        pista2.innerHTML = '<button class="btn btn-outline-secondary w-25 m-3 col">Pista 2</button>';
+        pista2.addEventListener('click', function () {
+            selected_pista = "2";
+            console.log(selected_pista);
+        });
+    }
+}
+
+function calcularRandom() {
+    random1 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+    random2 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+    random3 = Math.floor(Math.random() * (11 - 0 + 1) + 0);
+}
